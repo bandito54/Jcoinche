0b075051854de2a154db1d84fff1811d76b85083
Thomas BARIC <Thomas BARIC@10.18.207.99>
2016-11-24 17:21:49 +0100

    random card

--- src/main/java/cards/Cards.java
+++ src/main/java/cards/Cards.java
@@ -1,34 +1,31 @@
 package cards;
 
 public class Cards
-	{
-	public static void main(String[] args)
-	{
-		set_deck();
-		int i = 0;
-		while (i != 32)
-		{
-			System.out.println(deck[i]);
-			i++;
-		}
-	
-	}
-		private static String[] deck = new String[32];
-		
-		private static void set_deck()
-		{
-		    int i = 0;
-		    while (i != 32)
-			{
-		    	if (i <= 7)
-		    		deck[i] = "heart" + i;
-		    	else if (i >= 8 && i <= 16)
-		    		deck[i] = "diamond" + i;
-		    	else if (i >= 17 && i <= 25)
-		    		deck[i] = "spade" + i;
-		    	else if (i >= 26 && i <= 32)
-		    		deck[i] = "club" + i;
-				i++;
-			}
-		}
-	}
+{
+	 public static void main(String[] args)
+	 {
+		    int[] deck = new int[32];
+		    String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
+		    String[] ranks = {"Ace", "7", "8", "9", "10", "Jack", "Queen", "King"};
+
+		    // Initialize cards
+		    for (int i = 0; i < deck.length; i++) {
+		      deck[i] = i;
+		    }
+
+		    // Shuffle the cards
+		    for (int i = 0; i < deck.length; i++) {
+		      int index = (int)(Math.random() * deck.length);
+		      int temp = deck[i];
+		      deck[i] = deck[index];
+		      deck[index] = temp;
+		    }
+
+		    // Display the all the cards
+		    for (int i = 0; i < 32; i++) {
+		      String suit = suits[deck[i] / 8];
+		      String rank = ranks[deck[i] % 8];
+		      System.out.println( rank + " of " + suit);
+		    }
+		  }
+}
